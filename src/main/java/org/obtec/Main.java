304eec145d210dca7fab4c710bd51f87b2be2126
weird <ii3289857@gmail.com>
2024-02-24 01:03:51 +0900

    Update Main.java

--- src/main/java/org/obtec/Main.java
+++ src/main/java/org/obtec/Main.java
@@ -1,7 +1,18 @@
 package org.obtec;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws NumberFormatException, IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	int num = Integer.parseInt(br.readLine());
+    	bw.write(num * (num + 1) / 2 + "\n");
+    	bw.close();
+    	br.close();
     }
 }
