d7a743d65390a5cc54ae0360ca1d8994fc69723e
weird <ii3289857@gmail.com>
2024-02-26 18:45:03 +0900

    init

---
+++ src/main/java/com/obtec/Shacartesian/ShacartesianApplication.java
@@ -0,0 +1,13 @@
+package com.obtec.Shacartesian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShacartesianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShacartesianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obtec/Shacartesian/ShacartesianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obtec.Shacartesian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShacartesianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
